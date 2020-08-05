36d3e32567562d071f0e67ac82512607e95f93e4
Aderson Rangel <adersonrangel@gmail.com>
2020-08-05 16:02:23 -0500

    [Add] Update order information

---
+++ blazor-server-buildings/Models/OrderUpdateModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazor_server_buildings.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to a max of 20 characters")]
+        [MinLength(3, ErrorMessage = "you need to enter a least 3 characters for an order name")]
+        [DisplayName("Name for the Order")]
+        public string OrderName { get; set; }
+    }
+}
